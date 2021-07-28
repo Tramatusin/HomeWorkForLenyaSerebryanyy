9b02bc608769e4fa6dce9fdd40d13b6667fc39c9
Vi <krausz.st@gmail.com>
2021-07-28 21:45:25 +0300

    Setup collectionView

--- DrawWithLenya/DrawWithLenya/Controller/ExtensionForVC.swift
+++ DrawWithLenya/DrawWithLenya/Controller/ExtensionForVC.swift
@@ -10,20 +10,35 @@ import UIKit
 extension ViewController: UICollectionViewDataSource, UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
 //        PictureStash.pictures.count
-        5
+        15
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if indexPath.row == 0 {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "New", for: indexPath) as! NewFileViewCell
+            return cell
+        }
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Picture", for: indexPath) as! PictureViewCell
         return cell
     }
     
+    func collectionView(_ collectionView: UICollectionView, didEndDisplaying cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        if indexPath.row == 1 {
+            navigationItem.rightBarButtonItem = nil
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        if indexPath.row == 1 {
+            navigationItem.rightBarButtonItem = rightBarButtonItem
+        }
+    }
 }
 
 extension ViewController: UICollectionViewDelegateFlowLayout {
 
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        CGSize(width: 200, height: 200)
+        CGSize(width: view.bounds.width/2 - 1, height: view.bounds.width/2 - 1)
     }
 }
 
